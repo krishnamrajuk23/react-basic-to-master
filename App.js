435af9d6918b506fb3333c5b6f7bdb2f9d039071
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2023-03-25 20:28:31 +0530

    initial commit with part 1

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", { className: "heading" }, "this is my end to end understanding");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); 
---
+++ README.md
@@ -0,0 +1,1 @@
+# React learning basic to master
